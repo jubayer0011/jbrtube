e13294887945cb27255266739b6d706288397a45
Jubayer <99335368+jubayer0011@users.noreply.github.com>
2025-10-04 13:36:54 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const reply1998 = document.querySelector(".list1");
+const reply1988 = document.querySelector(".list1");
 const sultansuleman = document.querySelector(".list2");
 
-reply1998.addEventListener("click", function() {
-    window.location.href = "watch.html";
+reply1988.addEventListener("click", function() {
+    const watch = "reply1988"
+    window.location.href = "watch.html?data=" + watch;
 });
