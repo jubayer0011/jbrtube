75b8fc33cacd1ec11b2cf079301e7d2edfc43146
Jubayer <99335368+jubayer0011@users.noreply.github.com>
2025-10-04 10:12:43 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const reply1998 = document.querySelector(".list1");
+const sultansuleman = document.querySelector(".list2");
+
+reply1998.addEventListener("click", function() {
+    window.location.href = "watch.html";
+});
